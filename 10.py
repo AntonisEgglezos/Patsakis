4c73eb0c5622ebcc0877ff17d9c3c5554d1db2b3
AntonisEgglezos <47675551+AntonisEgglezos@users.noreply.github.com>
2019-02-17 19:17:50 +0200

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,12 @@
+﻿from bs4 import BeautifulSoup
+import requests
+URL=input("Δωσε μια διευθυνση")
+page=requests.get(URL)
+code=BeautifulSoup(page.text, 'lxml')
+newlines,links=0,0
+link=code.find_all('a')
+links=len(link)
+br=code.find_all('br')
+p=code.find_all('p')
+newlines=len(p)+len(br)
+print("Στην διευθυνση υπαρχουν",links,"συνδεσμοι και",newlines,"αλλαγες γραμης")
